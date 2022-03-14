c8100e705b9303b208b1f9d2c650c07619d9fd14
ShadowUser17 <linuxforce17@gmail.com>
2022-03-14 16:28:45 +0200

    Changes to be committed: 	new file:   get-debian-cloud.py

---
+++ get-debian-cloud.py
@@ -0,0 +1,54 @@
+#!/usr/bin/env python3
+from re import compile as re_compile
+from urllib import parse as urllib
+from html import parser as html
+from os import path as filepath
+from urllib import request
+
+
+class Parser(html.HTMLParser):
+    url_base = ''
+    url_list = []
+
+    def handle_starttag(self, tag: str, attrs: list) -> None:
+        if tag == 'a':
+            attrs = dict(attrs)
+            item = attrs.get('href')
+
+            if item:
+                self.url_list.append(urllib.urljoin(self.url_base, item))
+
+
+def get_url_data(url: str) -> list:
+    with request.urlopen(url) as req:
+        parser = Parser()
+        parser.url_base = url
+        parser.feed(req.read().decode())
+        return parser.url_list.copy()
+
+
+if __name__ == '__main__':
+    url_regex = re_compile('^.*-(\d+)\/$')
+    url_base = 'https://cloud.debian.org/images/cloud/bullseye/daily/'
+
+    url_list = get_url_data(url_base)
+    url_list = list(filter(lambda it: url_regex.match(it), url_list))
+
+    if url_list:
+        url_regex = re_compile('.*-(generic-amd64)-.*\.qcow2$')
+        url_base = urllib.urlparse(url_list[-1])
+        url_base = url_base.geturl()
+
+        url_list = url_list = get_url_data(url_base)
+        url_list = list(filter(lambda it: url_regex.match(it), url_list))
+
+        if url_list:
+            url = urllib.urlparse(url_list[-1])
+            filename = filepath.basename(url.path)
+
+            if not filepath.exists(filename):
+                print('Start download: {}'.format(filename))
+                (filename, _) = request.urlretrieve(url.geturl(), filename)
+
+            else:
+                print('File is exist: {}'.format(filename))
